7de01fb580ee84a2a47fd2cd651adcbb48165e88
Alex Rune Berg <arberg@gmail.com>
2023-03-07 20:57:07 +0100

    Added function for counting characters

--- src/main/java/net/sharplab/epubtranslator/core/service/EPubTranslatorService.kt
+++ src/main/java/net/sharplab/epubtranslator/core/service/EPubTranslatorService.kt
@@ -3,5 +3,6 @@ package net.sharplab.epubtranslator.core.service
 import net.sharplab.epubtranslator.core.model.EPubFile
 
 interface EPubTranslatorService {
+    fun countCharacters(ePubFile: EPubFile): Map<String, Int>
     fun translate(ePubFile: EPubFile, srcLang: String, dstLang: String): Pair<EPubFile, TranslationFailure?>
 }
